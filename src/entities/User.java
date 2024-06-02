52249a840cf40193de86943ee290f8a2825194a0
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 20:20:33 -0300

    creates a simple constructor to user class

--- src/entities/User.java
+++ src/entities/User.java
@@ -22,6 +22,12 @@ public class User {
     this.phoneNumber = phoneNumber;
   }
 
+  public User(String name, String email, String password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
   public String getName() {
     return name;
   }
