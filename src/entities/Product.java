c0bfdbe7e09e5936932954be739187f2fcbbb7d2
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 18:23:57 -0300

    creates the product class

---
+++ src/entities/Product.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Product {
+  private String name;
+  private Double price;
+  private Integer stock;
+
+  public Product(String name, Double price, Integer stock) {
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Integer getStock() {
+    return stock;
+  }
+
+  public void setStock(Integer stock) {
+    this.stock = stock;
+  }
+}
