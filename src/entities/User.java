16c4df94859734b7e78f76de0918cbe6c1069291
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 18:14:33 -0300

    implements address class in user class

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,11 +1,16 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
   private String name;
   private String email;
   private String password;
   private Cpf cpf;
   private String phoneNumber;
+
+  private List<Address> addresses = new ArrayList<>();
 
   public User(String name, String email, String password, String cpf, String phoneNumber) {
     this.name = name;
@@ -33,5 +38,9 @@ public class User {
 
   public String getPhoneNumber() {
     return phoneNumber;
+  }
+
+  public void addAddress(Address newAddress) {
+    addresses.add(newAddress);
   }
 }
