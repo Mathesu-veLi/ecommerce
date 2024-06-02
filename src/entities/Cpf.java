e48268a02033f829f295d620ae783afc828f6d5f
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 17:18:22 -0300

    creates a class to validate cpf

---
+++ src/entities/Cpf.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class Cpf {
+  private String formattedCpf;
+
+  public Cpf(String originalCpf) {
+    this.formattedCpf = originalCpf.replaceAll("\\D", "");
+  }
+
+  public boolean validate() {
+    if (formattedCpf.length() != 11) return false;
+    
+    String cpfWithoutFinalDigits = formattedCpf.substring(0, formattedCpf.length() - 2);
+
+    String newCpf = cpfWithoutFinalDigits;
+    for (int i = 0; i < 2; i++) {
+      newCpf += discoverDigit(newCpf);
+    }
+
+    return newCpf.equals(formattedCpf);
+  }
+
+  private String discoverDigit(String cpf) {
+    int sum = 0;
+    for (int i = 0, multiplier = cpf.length() + 1; i < cpf.length(); i++, multiplier--) {
+      int actualDigit = Character.getNumericValue(cpf.charAt(i));
+      sum += actualDigit * multiplier;
+    }
+
+    int digit = (sum * 10) % 11;
+    if (digit > 9) digit = 0;
+
+    return String.valueOf(digit);
+  }
+}
