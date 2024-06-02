db089d468bf0b6ab9fa5c17e69a316537bfebde6
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 18:17:17 -0300

    creates getters and setters to the address class

--- src/entities/Address.java
+++ src/entities/Address.java
@@ -9,9 +9,9 @@ public class Address {
   private String complement;
   private String referencePoint;
 
-  public Address(String contry, String state, String city, String street, String zipCode, String complement,
+  public Address(String country, String state, String city, String street, String zipCode, String complement,
       String referencePoint) {
-    this.country = contry;
+    this.country = country;
     this.state = state;
     this.city = city;
     this.street = street;
@@ -26,5 +26,61 @@ public class Address {
     this.city = city;
     this.street = street;
     this.zipCode = zipCode;
-  }  
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public String getComplement() {
+    return complement;
+  }
+
+  public void setComplement(String complement) {
+    this.complement = complement;
+  }
+
+  public String getReferencePoint() {
+    return referencePoint;
+  }
+
+  public void setReferencePoint(String referencePoint) {
+    this.referencePoint = referencePoint;
+  }
 }
