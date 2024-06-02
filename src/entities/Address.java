81eda018e5c56e801d62dec9e857709dc29086a0
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 18:14:16 -0300

    creates a address class

---
+++ src/entities/Address.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Address {
+  private String contry;
+  private String state;
+  private String city;
+  private String street;
+  private String zipCode;
+  private String complement;
+  private String referencePoint;
+}
