ac8bb2782c215f3ac01f9766012f1fc8bd783b67
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 18:16:43 -0300

    creates constructors to address class

--- src/entities/Address.java
+++ src/entities/Address.java
@@ -1,11 +1,30 @@
 package entities;
 
 public class Address {
-  private String contry;
+  private String country;
   private String state;
   private String city;
   private String street;
   private String zipCode;
   private String complement;
   private String referencePoint;
+
+  public Address(String contry, String state, String city, String street, String zipCode, String complement,
+      String referencePoint) {
+    this.country = contry;
+    this.state = state;
+    this.city = city;
+    this.street = street;
+    this.zipCode = zipCode;
+    this.complement = complement;
+    this.referencePoint = referencePoint;
+  }
+
+  public Address(String country, String state, String city, String street, String zipCode) {
+    this.country = country;
+    this.state = state;
+    this.city = city;
+    this.street = street;
+    this.zipCode = zipCode;
+  }  
 }
