868570ee344fded370b624baa53e2d35446ea253
Mathesu veLi <matheuslevit@gmail.com>
2024-06-02 21:22:42 -0300

    main code test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import entities.Product;
+import entities.ProductManager;
+import entities.User;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ProductManager productManager = new ProductManager();
+        Product product1 = new Product("Camisa do CV", 40.00, 2);
+
+        productManager.addProduct(product1);
+
+        User user = new User("Matheus", "matheuslevit@gmail.com", "veli15");
+
+        user.cart.addProduct(productManager, product1);
     }
 }
