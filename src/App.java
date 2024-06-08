f633b859076cdbff6a0adf7ff141b24481dd118d
Matheus <matheuslevit@gmail.com>
2024-06-08 18:37:56 -0300

    change the user instance to a customer instance

--- src/App.java
+++ src/App.java
@@ -1,15 +1,15 @@
 import entities.Product;
 import entities.ProductManager;
-import entities.User;
+import entities.Users.Customer;
 
 public class App {
     public static void main(String[] args) throws Exception {
         ProductManager productManager = new ProductManager();
-        Product product1 = new Product("Camisa do CV", 40.00, 2);
-
+        Product product1 = new Product("Camisa do Megadeth", 40.00, 2);
+        
         productManager.addProduct(product1);
 
-        User user = new User("Matheus", "matheuslevit@gmail.com", "veli15");
+        Customer user = new Customer("Matheus", "matheuslevit@gmail.com", "veli15");
 
         user.cart.addProduct(productManager, product1);
     }
