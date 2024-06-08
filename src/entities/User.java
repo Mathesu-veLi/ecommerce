82181b11ae11525ac1fd383b3aee24e01e34dcc7
Matheus <matheuslevit@gmail.com>
2024-06-08 17:51:36 -0300

    transform the user class in a abstract class

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,38 +1,12 @@
 package entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class User {
+public abstract class User {
   private String id;
   // TODO: Creates a id using a java uuid generator
   private String name;
   private String email;
   private String password;
   // TODO: Creates a hash of password
-  private Cpf cpf;
-  private String phoneNumber;
-
-  private List<Address> addresses = new ArrayList<>();
-
-  public Cart cart = new Cart();
-
-  public User(String name, String email, String password, Cpf cpf, String phoneNumber, List<Address> addresses) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.cpf = cpf;
-    this.phoneNumber = phoneNumber;
-    this.addresses = addresses;
-  }
-
-  public User(String name, String email, String password, String cpf, String phoneNumber) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.cpf = new Cpf(cpf);
-    this.phoneNumber = phoneNumber;
-  }
 
   public User(String name, String email, String password) {
     this.name = name;
@@ -53,18 +27,6 @@ public class User {
 
   public String getEmail() {
     return email;
-  }
-
-  public Cpf getCpf() {
-    return cpf;
-  }
-
-  public String getPhoneNumber() {
-    return phoneNumber;
-  }
-
-  public void addAddress(Address newAddress) {
-    addresses.add(newAddress);
   }
 }
 
