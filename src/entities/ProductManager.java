65b88cce3f52a70fd30c4419487b7aaa366cde00
Matheus <matheuslevit@gmail.com>
2024-06-08 18:42:12 -0300

    creates a function to get a product by uuid

--- src/entities/ProductManager.java
+++ src/entities/ProductManager.java
@@ -2,6 +2,7 @@ package entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductManager {
   private List<Product> products = new ArrayList<>();
@@ -17,6 +18,12 @@ public class ProductManager {
 
   public List<Product> getProducts() {
     return products;
+  }
+
+  public Product getProduct(String Uuid) {
+    Optional<Product> productFound = products.stream().filter(product -> product.getUuid() == Uuid).findFirst();
+    return productFound.get();
+    // FIXME: Throws a error when the productFound is empty
   }
 
   public void addProduct(Product newProduct) {
